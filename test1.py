6ca866aadc0446171edb40a84668665160a21c15
luka <luka.gerlach@gmx.de>
2020-03-22 17:51:22 +0100

    test

---
+++ test1.py
@@ -0,0 +1,12 @@
+a = [[0, 1, 1], [1, 0, 0], [1, 0, 0]]
+
+def knotengrad(x):
+    for i in x:
+        grad = 0
+        for j in i:
+            if j == 1:
+                grad += 1
+        print(grad)
+
+
+knotengrad(a)
